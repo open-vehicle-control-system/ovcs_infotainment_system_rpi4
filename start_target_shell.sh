74af4b7be9d5c84ef2eac249372171c3ba3c72b2
Marc Lainez <marc.lainez@gmail.com>
2023-10-27 19:32:02 +0200

    Add easy shell script to get into target shell and use menuconfig

---
+++ start_target_shell.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+./deps/nerves_system_br/create-build.sh nerves_defconfig .nerves/artifacts/ovcs_infotainment_system_rpi4-portable-1.24.1 >/dev/null
+cd .nerves/artifacts/ovcs_infotainment_system_rpi4-portable-1.24.1
+echo "*** You are now in your target shell, to go back type exit or press CTRL-d"
+echo "*** Use make menuconfig to enter buildroot menu"
+echo "*** Use make savedefconfig when you're done for your changes to make effect" 
+exec bash
