fc70b0ad08789255172023d646b621b45ddd6622
Marc Lainez <marc.lainez@gmail.com>
2024-04-26 12:35:55 +0200

    Adapt script to match system version

--- start_target_shell.sh
+++ start_target_shell.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
-./deps/nerves_system_br/create-build.sh nerves_defconfig .nerves/artifacts/ovcs_infotainment_system_rpi4-portable-1.27.0 >/dev/null
-cd .nerves/artifacts/ovcs_infotainment_system_rpi4-portable-1.27.0
+VERSION=$(head -1 ./VERSION)
+
+./deps/nerves_system_br/create-build.sh nerves_defconfig .nerves/artifacts/ovcs_infotainment_system_rpi4-portable-${VERSION} >/dev/null
+cd .nerves/artifacts/ovcs_infotainment_system_rpi4-portable-${VERSION}
 echo "*** You are now in your target shell, to go back type exit or press CTRL-d"
 echo "*** Use make menuconfig to enter buildroot menu"
 echo "*** Use make savedefconfig when you're done for your changes to make effect" 
